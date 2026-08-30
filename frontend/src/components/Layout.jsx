26656a93139e4b561e641dddc1810a0ae736df33
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-08-30 20:45:56 +0700

    style: style homepage and cursor

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer";
+import { useEffect } from "react";
+
+import fluidCursor from "../contexts/use-FluidCursor";
 
 const Layout = () => {
+  useEffect(() => {
+    fluidCursor();
+  }, []);
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className="bg-gbg-3">
+        <Outlet />
+        <div className="fixed top-0 left-0 z-2 pointer-events-none">
+          <canvas id="fluid" className="w-screen h-screen" />
+        </div>
+      </div>
       <Footer />
     </>
   );
