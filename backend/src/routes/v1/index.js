c33bcebd4072584db9e8be195037bf33686ea247
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-09-09 19:51:50 +0700

    feat: add bcrypt on user route

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { userRouter } from "./users.routes";
+import { productRouter } from "./products.routes";
 
 export const router = Router();
 
 router.use('/users', userRouter)
-router.use('/products',)
+router.use('/products', productRouter)
--- backend/src/routes/v1/users.routes.js
+++ backend/src/routes/v1/users.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { User } from "../../models/user.model";
+import bcrypt from 'bcrypt'
 
 export const userRouter = Router();
 
@@ -14,5 +15,16 @@ userRouter.get('/', async (req, res) => {
         return res.json({
             message: `ERROR: ${error}`
         })
+    }
+})
+
+userRouter.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const salt = await bcrypt.genSalt(10)
+        const newPassword = await bcrypt.hash(password, salt)
+        console.log(newPassword)
+    } catch (error) {
+        console.log(error)
     }
 })
