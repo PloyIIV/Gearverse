d78a3f1ab1eb0aca77bb106d07c6a2f3b6291d79
B6001186 <atcharapornok@gmail.com>
2026-09-17 13:59:03 +0700

    modified review, product model and review route

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -1,13 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-  user_id: { type: Schema.ObjectId, ref: "User", required: true },
-  product_id: { type: Schema.ObjectId, ref: "Product", required: true },
-  orderitem_id: { type: Schema.ObjectId, ref: "OrderItem" },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  comment: { type: String, trim: true },
-}, {
-  timestamps: true,
-});
+const reviewSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    orderitem_id: { type: Schema.ObjectId, ref: "OrderItem" },
+    rating: { type: Number, min: 1, max: 5, required: true },
+    comment: { type: String, trim: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const Review = mongoose.model("Review", reviewSchema);
